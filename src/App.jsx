caa61faae42c3030a73a0c33609b83ecb83e4043
Rokibul Alom <itismehey975@gmail.com>
2024-10-22 09:23:24 +0600

    second commit - in this commit we createfake data json files , comoponents folder and inside this we create a blogs folder and fetch the fake data into it

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Blogs from './Components/Blogs/Blogs'
 
 function App() {
 
   return (
     <>
       <h1 className='bg-red-400 text-2xl'>Vite + React</h1>
+      <Blogs></Blogs>
     </>
   )
 }
